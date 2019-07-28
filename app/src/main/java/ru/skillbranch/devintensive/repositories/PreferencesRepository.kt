c2311463dd1b1535699641045e2f03f191012c39
Aleksey Rodionov <taker.ram512@gmail.com>
2019-07-28 21:16:48 +0300

    Third part

---
+++ app/src/main/java/ru/skillbranch/devintensive/repositories/PreferencesRepository.kt
@@ -0,0 +1,14 @@
+package ru.skillbranch.devintensive.repositories
+
+import ru.skillbranch.devintensive.models.Profile
+
+object PreferencesRepository {
+    fun getProfile(): Profile? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun saveProfile(profile: Profile) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
