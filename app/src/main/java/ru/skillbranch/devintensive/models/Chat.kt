0ba3a91b2e27d20a7368524cd6f084c419b9582b
Aleksey Rodionov <taker.ram512@gmail.com>
2019-08-31 11:49:33 +0300

    First part

--- app/src/main/java/ru/skillbranch/devintensive/models/Chat.kt
+++
@@ -1,9 +0,0 @@
-package ru.skillbranch.devintensive.models
-
-class Chat(
-    val id: String,
-    val memvers: MutableList<User> = mutableListOf(),
-    val messages: MutableList<BaseMessage> = mutableListOf()
-) {
-
-}
