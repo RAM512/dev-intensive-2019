d7b5fb77172c2f38def222307dd697949c115c94
Aleksey Rodionov <taker.ram512@gmail.com>
2019-08-31 16:03:42 +0300

    Second part

--- app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.ui.adapters.ChatAdapter
+import ru.skillbranch.devintensive.ui.adapters.ChatItemTouchHelperCallback
 import ru.skillbranch.devintensive.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -37,11 +38,22 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(rv_chat_list, "Clicked on ${it.title}", Snackbar.LENGTH_LONG).show()
         }
         val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        val touchCallback = ChatItemTouchHelperCallback(chatAdapter) {
+            viewModel.addToArchive(it.id)
+            Snackbar.make(rv_chat_list, "Вы точно хотите добавить ${it.title} в архив?", Snackbar.LENGTH_LONG).show()
+        }
 
         with (rv_chat_list) {
             adapter = chatAdapter
             layoutManager = LinearLayoutManager(this@MainActivity)
             addItemDecoration(divider)
+        }
+
+        val touchHelper = ItemTouchHelper(touchCallback)
+        touchHelper.attachToRecyclerView(rv_chat_list)
+
+        fab.setOnClickListener {
+            viewModel.addItems()
         }
     }
 
